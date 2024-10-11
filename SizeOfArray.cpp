09598b2dddf87b27e4cdcb6ace775665e5666fdd
Christopher Sefcik <cdsefcik@gmail.com>
2024-10-11 18:47:36 -0400

    Update SizeOfArray.cpp

--- SizeOfArray.cpp
+++ SizeOfArray.cpp
@@ -13,7 +13,7 @@ int main() {
 
 	int arraySize = (sizeof(array1) / sizeof(array1[0]));
 
-	cout << "The size of the array is: " << arraySize << endl;
+	cout << "The length of the array is: " << arraySize << endl;
 	
 
 	return EXIT_SUCCESS;
