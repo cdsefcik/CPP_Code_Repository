51990e9ba090c1a990b33148ccff8b8b24fc6e43
Christopher Sefcik <cdsefcik@gmail.com>
2025-01-31 09:54:24 -0500

    Create ConditionalCompilation.h

---
+++ ConditionalCompilation.h
@@ -0,0 +1,15 @@
+//Here are some header guards used for conditional compilation
+
+//EXE different header files for linux or windows:
+
+#ifdef WINDOWS
+    #include "my_windows_header.h
+#else
+    #include "my_linux_header.h"
+#endif
+
+//Stops header files from being included twice:
+#ifndef MY_WINDOWS_HEADER
+#define MY_WINDOWS_HEADER
+
+#endif
