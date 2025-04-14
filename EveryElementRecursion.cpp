ad431adda279d102d42a605cbcb4d9d6d10852fa
Christopher Sefcik <cdsefcik@gmail.com>
2025-04-14 09:27:46 -0400

    Update EveryElementRecursion.cpp

--- EveryElementRecursion.cpp
+++ EveryElementRecursion.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
 /*
-Prints every element in an array.
+Prints every position with the value of x.
+The second function saves every position in an array with the value of x.
 
 */
 
@@ -18,5 +20,18 @@ void printAllPos(int a[], int n, int x, int i) {
 	}
 
 	printAllPos(a, n, x, i+1);
+
+}
+
+void saveAllPos(int a[], int n, int x, int i, vector<int> &ans) {
+	if (i == n) {
+		return;
+	}
+
+	if (a[i] == x) {
+		ans.push_back(i);
+	}
+
+	saveAllPos(a, n, x, i + 1, ans);
 
 }
